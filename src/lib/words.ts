27292515d79e7083b4d3a4d7cd3c1b9ee73c0854
Dimitar <dimitarduino@gmail.com>
2022-02-12 18:26:28 +0100

    prvo i posledno

---
+++ src/lib/words.ts
@@ -0,0 +1,51 @@
+import { WORDS } from '../constants/wordlist'
+import { VALID_GUESSES } from '../constants/valid-guesses'
+
+// January 1, 2022 Game Epoch
+const EPOCH_START = 1640991600000
+const MS_IN_DAY = 86400000
+let zborotNaDenotIndex = -1;
+
+export const isWordInWordList = (word: string) => {
+    return WORDS.includes(word.toLowerCase()) || VALID_GUESSES.includes(word.toLowerCase())
+}
+
+export const getWordOfDay = () => {
+    return WORDS[getWordOfDayIndex()].toUpperCase()
+}
+
+export const isWinningWord = (word: string) => {
+    return getWordOfDay() === word
+}
+
+export const getWordOfDayIndex = () => {
+    
+    if (zborotNaDenotIndex == -1) {
+        const now = Date.now()
+        
+        zborotNaDenotIndex = Math.round(Math.random() * WORDS.length);
+
+        return zborotNaDenotIndex;
+    } else {
+        return zborotNaDenotIndex;
+    }
+}
+
+export const getTimeUntilNextWord = () => {
+    const solutionIndex = getWordOfDayIndex()
+    const time = EPOCH_START + (solutionIndex + 1) * MS_IN_DAY - Date.now()
+    const date = new Date(time)
+    return {
+        hours: (24 + (date.getHours() - 1)) % 24,
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+        solutionIndex: solutionIndex,
+    }
+}
+export type Time = {
+    hours: number
+    minutes: number
+    seconds: number
+}
+
+export const formatTime = (time: number) => (time >= 10 ? `${time}` : `0${time}`)
